a97909b1b45116f9840eada2d58d73c589645bdb
apla02 <1873@holbertonschool.com>
2020-12-14 13:18:20 -0500

    create a new database and user

---
+++ setup_mysql_test.sql
@@ -0,0 +1,5 @@
+--create database hbnb_test_db, user 'hbnb_test'@'localhost' and set privileges
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
