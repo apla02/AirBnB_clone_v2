96144e64e384c538ebc095154b2269415520ec72
apla02 <1873@holbertonschool.com>
2020-12-14 13:28:09 -0500

    new database and user

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,4 +1,4 @@
---create database hbnb_test_db, user 'hbnb_test'@'localhost' and set privileges
+--create database hbnb_test_db, user hbnb_test@localhost and set privileges
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
