04a537e965f7971ab25ad2bafa3c9963651af02c
apla02 <1873@holbertonschool.com>
2020-12-15 16:58:45 -0500

    updating user class

--- models/state.py
+++ models/state.py
@@ -4,14 +4,13 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from models.city import City
-import models
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade='delete')
+    cities = relationship("City", backref="state", cascade='all, delete')
 
     @property
     def cities(self):
