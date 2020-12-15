5d6cc2589f619f5d5cd8f6ce77c5fa28390adcc5
apla02 <1873@holbertonschool.com>
2020-12-15 18:16:12 -0500

    fixing nullable condition

--- models/user.py
+++ models/user.py
@@ -11,6 +11,6 @@ class User(BaseModel, Base):
     __tablename__ = 'users'
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
     places = relationship("Place", backref="users", cascade="delete")
