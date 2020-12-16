05a7e78238926d2a54517edd94f270fd7647aef8
apla02 <1873@holbertonschool.com>
2020-12-16 14:18:10 -0500

    include os

--- models/place.py
+++ models/place.py
@@ -4,7 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, Float, String, ForeignKey, Table
 from models.review import Review
 from sqlalchemy.orm import relationship
-
+from os import getenv
 
 place_amenity = Table('place_amenity', Base.metadata, Column(
     'place_id', String(60), ForeignKey(
