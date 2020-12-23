0020e467f1525cd20cf384e7ed0247e49c9e066b
apla02 <apla02@gmail.com>
2020-12-22 20:23:06 -0500

    fixing requeriments

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,11 +2,8 @@
 # sets up your web servers for the deployment of web_static
 sudo apt-get update
 sudo apt-get -y install nginx
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/releases/test
 sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
 sudo echo 'hello' | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test /data/web_static/current 
 sudo chown  -R ubuntu:ubuntu /data/
