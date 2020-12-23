c2277b70e779442093934c0b08a42ddd88e71723
apla02 <apla02@gmail.com>
2020-12-22 20:04:43 -0500

    setup

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,7 +8,7 @@ sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 sudo echo 'hello' | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/current /data/web_static/releases/test/
+sudo ln -sf /data/web_static/releases/test /data/web_static/current 
 sudo chown  -R ubuntu:ubuntu /data/
 sudo sed -i '43 a\\tlocation \/hbnb_static\/ {\n\talias \/data\/web_static\/current\/;\n\t}\n ' /etc/nginx/sites-available/default
 sudo service nginx restart
