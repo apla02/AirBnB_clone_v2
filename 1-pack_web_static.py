1048cf9eb8e96df43464fe355e6fb42413f0e3b1
apla02 <apla02@gmail.com>
2020-12-23 00:04:27 -0500

    docstring y pep8

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,12 +2,18 @@
 from fabric.api import local
 from datetime import datetime
 
+
 def do_pack():
+    '''
+    script that generates a .tgz archive from the contents
+    of the web_static folder of your AirBnB Clone repo,
+    using the function do_pack
+    '''
     try:
         now_string = datetime.now().strftime('%Y%m%d%H%M%S')
-        filename= 'versions/web_static_'+ now_string + '.tgz'
+        filename = 'versions/web_static_' + now_string + '.tgz'
         local('mkdir -p versions')
         local('tar -cvzf {} web_static'.format(filename))
-        print('Packing web_static to versions/{}'.format(filename))
+        print('Packing web_static to {}'.format(filename))
     except:
         None
