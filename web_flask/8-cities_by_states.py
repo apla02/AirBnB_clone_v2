4f56c96471a1538cd387d5a4d1621cfbe0495d93
apla02 <apla02@gmail.com>
2021-01-19 14:11:05 -0500

    issues with output

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -15,24 +15,14 @@ def teardown(exception):
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    '''
-    list of states from a storage
-    '''
-    states = storage.all('State')
-    return render_template("7-states_list.html", states=states)
-
-
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
     '''
     list of cities by state
     '''
     states = storage.all('State')
-    cities = storage.all('City')
     return render_template(
-        "8-cities_by_states.html", states=states, cities=cities)
+        "8-cities_by_states.html", states=states)
 
 
 if __name__ == '__main__':
