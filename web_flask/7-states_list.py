2cddd2895438d5e81613ef708c3ee5d7d2c2d7d4
apla02 <apla02@gmail.com>
2021-01-19 12:51:14 -0500

    styling

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -23,6 +23,5 @@ def states_list():
     states = storage.all('State')
     return render_template("7-states_list.html", states=states)
 
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
