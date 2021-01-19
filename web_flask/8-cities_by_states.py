a0c06212a2c2dc64a0881d74406b647e15e5e723
apla02 <apla02@gmail.com>
2021-01-19 11:51:30 -0500

    filtering cities belongs to an state

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+'''
+script that starts a Flask web application
+'''
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    '''each request you must remove the current SQLAlchemy Session
+    '''
+    storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_statest():
+    '''
+    list of states
+    '''
+    states = storage.all('State')
+    cities = storage.all('City')
+    return render_template(
+        "8-cities_by_states.html", states=states, cities=cities)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
