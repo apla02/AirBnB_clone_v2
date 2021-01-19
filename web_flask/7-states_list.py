f5af9b2298db744f7f55a5b6934e6f45802112c6
apla02 <apla02@gmail.com>
2021-01-19 13:54:00 -0500

    updating permision

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-from flask import Flask, render_template
-from models import storage
-from models.state import State
 '''
 script that starts a Flask web application
 '''
+from flask import Flask, render_template
+from models import storage
+from models.state import State
 app = Flask(__name__)
 
 
